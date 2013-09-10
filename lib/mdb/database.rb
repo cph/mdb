0785244114822b5d81bdfe2a49f4fa9b1625dbc8
Bob Lail <bob.lail@cph.org>
2013-09-10 13:33:26 -0500

    [improvement] Supported streaming records from mdb-export's standard output (1h)

--- lib/mdb/database.rb
+++ lib/mdb/database.rb
@@ -1,20 +1,24 @@
 require 'tempfile'
+require 'shellwords'
+require 'open3'
+require 'csv'
+
 
 module Mdb
   class Database
     
     
     
-    def initialize(file)
+    def initialize(file, options={})
       raise FileDoesNotExistError, "\"#{file}\" does not exist" unless File.exist?(file)
       
       @file = file
-      @delim = '|' # We're going to assume no pipes in data
+      @delimiter = options.fetch :delimiter, '|'
     end
     
     
     
-    attr_reader :file
+    attr_reader :file, :delimiter
     
     
     
@@ -25,9 +29,7 @@ module Mdb
     
     
     def columns(table)
-      csv = read_csv(table)
-      first_line = csv[/^(.*)$/]
-      parse_columns(first_line)
+      open_csv(table) { |csv| csv.readline.map(&:to_sym) }
     end
     
     
@@ -43,14 +45,14 @@ module Mdb
     
     
     
-    # Returns an array of hashes. Each hash represents a record
+    # Yields a hash for each record
     def each_record(table, &block)
       columns = nil
       read_each(table) do |line|
         if columns
-          yield map_to_hash(line.split(@delim), columns)
+          yield Hash[columns.zip(line)]
         else
-          columns = parse_columns(line)
+          columns = line.map(&:to_sym)
         end
       end
     end
@@ -73,44 +75,37 @@ module Mdb
     
     
     
-    def parse_columns(line)
-      line.split(@delim).map {|name| name.empty? ? nil : name.to_sym }
-    end
-    
-    
-    
-    def map_to_hash(values, columns)
-      hash = {}
-      (0...columns.length).each do |i|
-        column = columns[i]
-        next if column.nil?
-        value = values[i]
-        hash[column] = value && value.delete("\"")
-      end
-      hash
-    end
-    
-    
-    
     def read_each(table, &block)
       count = 0
-      read_csv(table).each_line do |line|
-        yield line.chomp
-        count += 1
+      
+      open_csv(table) do |csv|
+        while line = csv.readline
+          yield line
+          count += 1
+        end
       end
+      
+      if count == 0
+        raise TableDoesNotExistError, "#{table.inspect} does not exist in #{file_name.inspect}" if !tables.member?(table.to_s)
+        raise Error, "An error occurred when reading #{table.inspect} in #{file_name.inspect}"
+      end
+      
       count # don't return the result of read_csv; discard it
     end
     
     
     
-    def parse(csv)
-      csv.split /\n/
+    def file_name
+      Shellwords.escape(file)
     end
     
     
     
-    def file_name
-      @file.gsub(' ', '\ ')
+    def open_csv(table)
+      command = "mdb-export -d #{Shellwords.escape(delimiter)} #{file_name} #{table}"
+      Open3.popen3(command) do |stdin, stdout, stderr|
+        yield CSV.new(stdout, col_sep: delimiter)
+      end
     end
     
     
@@ -118,6 +113,7 @@ module Mdb
     def execute(command)
       stdout = `#{command} 2> /dev/null`
       
+      # !todo: add fixture data and a test to prove this code
       if stdout.respond_to?(:force_encoding)
         stdout.force_encoding("Windows-1252")
         stdout.encode!("utf-8")
